5256d988141cde3a4825c401bf512e7d8ba3520a
Cezn.Vi <cezn.vi@gmail.com>
2024-08-25 19:02:26 +0300

    fix: - searchPanel

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Loader } from '@googlemaps/js-api-loader';
 import '../styles/homePage.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
 const HomePage: React.FC = () => {
     const searchInputRefFrom = useRef<HTMLInputElement>(null);
     const searchInputRefTo = useRef<HTMLInputElement>(null);
@@ -84,33 +85,40 @@ const HomePage: React.FC = () => {
             {/* Основний контент */}
             <Container fluid className="text-center middlePanel">
                 <h1 className="display-4 firstHeader">Low price</h1>
-                <p className="lead">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p className="lead">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                    magna aliqua.</p>
 
                 {/* Панель пошуку */}
                 <div className="my-4 searchPanel">
-                    <Form className="d-flex justify-content-center searchForm">
-                        <input ref={searchInputRefFrom} type="text" placeholder="Відправка з" className="me-2" />
-                        <input ref={searchInputRefTo} type="text" placeholder="Прямуєте до" className="me-2" />
-                        <input type="text" placeholder="Коли?" className="me-2" />
-                        <input type="text" placeholder="1 пасажир" className="me-2" />
-                        <Button variant="primary">Шукати</Button>
+                    <Form className="d-flex justify-content-center flex-wrap w-100 searchForm">
+                        <input ref={searchInputRefFrom} type="text" placeholder="Відправка з"
+                               className="me-2 flex-fill mb-2"/>
+                        <input ref={searchInputRefTo} type="text" placeholder="Прямуєте до"
+                               className="me-2 flex-fill mb-2"/>
+                        <input type="text" placeholder="Коли?" className="me-2 flex-fill mb-2"/>
+                        <input type="text" placeholder="1 пасажир" className="me-2 flex-fill mb-2"/>
+                        <Button variant="primary" className="mb-2">Шукати</Button>
                     </Form>
                 </div>
+
+
             </Container>
 
             {/* Нижній блок з картками */}
             <Container fluid className="mt-5">
                 <div className="d-flex justify-content-around cards">
-                    <div className="bg-white p-4 m-2" >
+                    <div className="bg-white p-4 m-2">
                         <h5>Вибирайте поїздки за низькою ціною</h5>
-                        <p>Куди б ви не їхали, автобусом чи з попутниками, знайдіть ідеальну поїздку з безлічі напрямків і маршрутів – і подорожуйте за низькими цінами.</p>
-                    </div>
-                    <div className="bg-white p-4 m-2" >
-                        <h5>Довіряйте своїм попутникам</h5>
-                        <p>Ми добре знаємо всіх своїх пасажирів і автобусних партнерів. Ми перевіряємо відгуки та профілі, щоб ви подорожували із впевненістю.</p>
+                        <p>Куди б ви не їхали, автобусом чи з попутниками, знайдіть ідеальну поїздку з безлічі напрямків
+                            і маршрутів – і подорожуйте за низькими цінами.</p>
                     </div>
                     <div className="bg-white p-4 m-2">
-                        <h5>Прокрутіть, клацніть, натисніть і забронюйте!</h5>
+                    <h5>Довіряйте своїм попутникам</h5>
+                        <p>Ми добре знаємо всіх своїх пасажирів і автобусних партнерів. Ми перевіряємо відгуки та
+                            профілі, щоб ви подорожували із впевненістю.</p>
+                    </div>
+                    <div className="bg-white p-4 m-2">
+                    <h5>Прокрутіть, клацніть, натисніть і забронюйте!</h5>
                         <p>Забронювати поїздку ще ніколи не було так легко! Завдяки потужному алгоритму наш застосунок знайде водія поруч із вами всього за кілька хвилин.</p>
                     </div>
                 </div>
