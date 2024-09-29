08359d5c9d4ab7aec6dd950108f3fc3f584eef8a
Sofia <sofapokl@gmail.com>
2024-09-29 15:59:55 +0300

    searchRes v0

--- react/src/pages/SearchResult.tsx
+++ react/src/pages/SearchResult.tsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Navbar from "../components/NavbarComponent.tsx";
 import Footer from "../components/main/Footer/Footer.tsx";
 import SearchPanel from "../components/SearchPanel.tsx";
+import { Card, Button, Form, Col, Row } from 'react-bootstrap';
+import "../styles/SearchResult.css";
 
 const SearchResult: React.FC = ()=>
 {
- return (
+    const [trips, setTrips] = useState<any[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [filters, setFilters] = useState({ departureTime: '', priceRange: '', trust: '', amenities: [] });
+
+    useEffect(() => {
+        const fetchTrips = async () => {
+            /*try {
+                const response = await fetch('/api/trips/getSearchTrip'); // Ваш реальний API
+                const data = await response.json();
+                setTrips(data);
+            } catch (error) {
+                console.error('Error fetching trips:', error);
+            } finally {
+                setLoading(false);
+            }*/
+        };
+
+        fetchTrips();
+    }, []);
+
+    const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = e.target;
+        setFilters({ ...filters, [name]: value });
+    };
+
+    return (
      <main className="main">
-        <Navbar/>
+         <Navbar/>
          <SearchPanel/>
+         <Options/>
          <Footer/>
      </main>
  );
