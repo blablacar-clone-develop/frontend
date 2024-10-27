5e3883ee2d397d6e408e7842c810dc3207608a13
Cezn.Vi <cezn.vi@gmail.com>
2024-10-27 13:08:18 +0200

    Add: - Page = ComingSoon.tsx - redirect on PayCard page on ComingSon page

---
+++ react/src/pages/ComingSoon.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavbarComponent from "../components/NavbarComponent.tsx";
+import Footer from "../components/main/Footer/Footer.tsx";
+
+
+const CommingSoon: React.FC = () => {
+
+    return (
+        <main className="main">
+            <NavbarComponent />
+
+            <Footer/>
+        </main>
+    );
+};
+
+export default CommingSoon;
